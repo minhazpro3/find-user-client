1c98ab57686dedb90c279106c582b5d05fab9f3e
minhazpro3 <minhazpro1@gmail.com>
2022-12-04 12:18:59 +0600

    project setup

---
+++ src/components/User_details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User_details = () => {
+  return <div>User_details</div>;
+};
+
+export default User_details;
---
+++ src/components/User_list.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User_list = () => {
+  return <div>User_list</div>;
+};
+
+export default User_list;
